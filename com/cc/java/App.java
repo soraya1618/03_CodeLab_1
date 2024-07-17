a8264cab07c11eabd302cfbc17e03b8f51668004
soraya1618 <sorayyakhawari@gmail.com>
2024-07-17 22:46:03 +0200

    get kontostand

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,11 +3,29 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-    
+        
+        Konto konto1 = new Konto(10000);
+        Konto konto2 = new Konto(10000);
+        Konto konto3 = new Konto(10000);
+
+        output("Konto1: " + konto1.getKontostand());
+        output("Konto2: " + konto2.getKontostand());
+        output("Konto3: " + konto3.getKontostand());
+
+        output("-------------------------");
+
+        konto1.setKontostand(konto1.getKontostand()*2);
+        konto2.setKontostand(konto2.getKontostand()*3);
+        konto3.setKontostand(konto3.getKontostand()*10);
+
+        output("Konto1: " + konto1.getKontostand());
+        output("Konto2: " + konto2.getKontostand());
+        output("konto3: " + konto3.getKontostand());
+        
     }
 
 
-    private static void output(String outputStr) {
+    private  static void output(String outputStr) {
         System.out.println(outputStr);
     } 
 
---
+++ com/cc/java/Konto.java
@@ -0,0 +1,17 @@
+package com.cc.java;
+
+public class Konto {
+
+    private int kontostand;
+
+    public Konto(int kontostand) {
+        this.kontostand = kontostand;
+    }
+    
+    public int getKontostand() {
+        return kontostand;
+    }
+    public void setKontostand(int kontostand){
+        this.kontostand = kontostand;
+    }
+}
